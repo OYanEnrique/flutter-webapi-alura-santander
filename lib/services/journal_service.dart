c657f19db83eece99f65e42e3254a2ec4e716f4c
OYanEnrique <enrique4work@gmail.com>
2026-01-04 15:30:23 -0300

    refactor: utiliza ApiConfig para configuração da URL base

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_webapi_first_course/config/api_config.dart';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/intercepted_client.dart';
+
 class JournalService {
-  // TODO: Substitua pelo IP da sua máquina local (ex: http://192.168.x.x:3000/)
-  static const String url = "http://localhost:3000/";
+  static String url = ApiConfig.baseUrl;
   static const String resource = "learnhttp/";
 
 http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
