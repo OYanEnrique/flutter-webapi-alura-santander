81049466ed08918dfc067e6d5843152e4183d901
OYanEnrique <enrique4work@gmail.com>
2026-01-05 16:02:13 -0300

    feat: implementa método register com serialização JSON e validação de resposta

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter_webapi_first_course/config/api_config.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/intercepted_client.dart';
 
 class JournalService {
   static String url = ApiConfig.baseUrl;
-  static const String resource = "learnhttp/";
+  static const String resource = "journals/";
 
 http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
@@ -13,11 +16,18 @@ http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()
     return "$url$resource";
   }
 
-  register(String content){
-    client.post(Uri.parse(getUrl()), 
-      body: {
-        "content": content
-        });
+ Future<bool> register(Journal journal) async {
+    String jsonJournal = json.encode(journal.toMap());
+    http.Response response = await client.post(Uri.parse(getUrl()), 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonJournal);
+    if(response.statusCode == 201){
+      print("Journal registrado com sucesso!");
+      return true;
+    } 
+    return false;
   }
   Future<String> get() async {
     http.Response response = await client.get(Uri.parse(getUrl()));
