db93a59d4e6c15681fc13f810a2fc83e4c41e889
OYanEnrique <enrique4work@gmail.com>
2026-01-06 15:25:34 -0300

    feat: adiciona função refreshFunction aos cards para atualização automática

--- lib/screens/home_screen/widgets/journal_card.dart
+++ lib/screens/home_screen/widgets/journal_card.dart
@@ -6,7 +6,8 @@ import 'package:uuid/uuid.dart';
 class JournalCard extends StatelessWidget {
   final Journal? journal;
   final DateTime showedDate;
-  const JournalCard({Key? key, this.journal, required this.showedDate})
+  final Function refreshFunction;
+  const JournalCard({Key? key, this.journal, required this.showedDate, required this.refreshFunction})
       : super(key: key);
 
   @override
@@ -55,7 +56,7 @@ class JournalCard extends StatelessWidget {
                       ),
                     ),
                     padding: const EdgeInsets.all(8),
-                    child: Text(WeekDay(journal!.createdAt.weekday).short),
+                    child: Text(WeekDay(journal!.createdAt).short),
                   ),
                 ],
               ),
@@ -87,7 +88,7 @@ class JournalCard extends StatelessWidget {
           height: 115,
           alignment: Alignment.center,
           child: Text(
-            "${WeekDay(showedDate.weekday).short} - ${showedDate.day}",
+            "${WeekDay(showedDate).short} - ${showedDate.day}",
             style: const TextStyle(fontSize: 12),
             textAlign: TextAlign.center,
           ),
@@ -105,6 +106,7 @@ class JournalCard extends StatelessWidget {
         updatedAt: showedDate,
       ),
     ).then((value){
+      refreshFunction();
       if(value != null && value == true){
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
