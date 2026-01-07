a6a470e00be6856d7156904f90a333da2f6ed402
OYanEnrique <enrique4work@gmail.com>
2026-01-07 15:48:38 -0300

    feat: implementa método edit no JournalService e lógica de edição na tela AddJournal

--- lib/screens/add_journal_screen/add_journal_screen.dart
+++ lib/screens/add_journal_screen/add_journal_screen.dart
@@ -5,12 +5,14 @@ import 'package:flutter_webapi_first_course/services/journal_service.dart';
 
 class AddJournalScreen extends StatelessWidget {
   final Journal journal;
-  AddJournalScreen({super.key, required this.journal});
+  final bool isEditing;
+  AddJournalScreen({super.key, required this.journal, required this.isEditing});
 
   final TextEditingController _contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    _contentController.text = journal.content;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -45,9 +47,16 @@ class AddJournalScreen extends StatelessWidget {
     journal.content = content;
 
     JournalService service = JournalService();
-    service.register(journal).then((value){
+    if (isEditing) {
+      service.register(journal).then((value){
        Navigator.pop(context, value);
     });
+    }else{
+      service.edit(journal.id, journal).then((value){
+       Navigator.pop(context, value);
+    });
+    }
+    
 
    
     
